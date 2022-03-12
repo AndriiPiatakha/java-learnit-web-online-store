897427975ab0411ee3a66ca7daf84b4cbc81dd6e
Andrey Pyatakha <andrey.pyatakha@gmail.com>
2022-03-12 16:48:44 +0200

    Hello World Servlet

---
+++ online-store.web/src/main/java/com/itbulls/learnit/onlinestore/web/servlets/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package com.itbulls.learnit.onlinestore.web.servlets;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().println("Hello World!");
+	}
+
+}
